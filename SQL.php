d4154fa01d7f0ae3ab97adb6088377167edad9bf
Jonathan Kim <33075340+jonathan-sj-kim@users.noreply.github.com>
2020-03-28 18:28:35 -0700

    Add files via upload

---
+++ SQL.php
@@ -0,0 +1,7 @@
+<html>
+
+<?php
+    echo "Hello world";
+    phpinfo(); // Print PHP version and config info
+?>
+</html>
---
+++ test.php
@@ -0,0 +1,11 @@
+<html>
+<?php
+	if ($c=OCILogon("ora_jonkim99", "a46095295", "dbhost.students.cs.ubc.ca:1522/stu")) {
+		echo "Successfully connected to Oracle.\n";
+		OCILogoff($c);
+	} else {
+		$err = OCIError();
+		echo "Oracle Connect Error " . $err['message'];
+	}
+?>
+</html>
