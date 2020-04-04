d2b8d9f34b73196fb2612b8004e4e1f748413a02
Gallant Tang <gallanttang@gmail.com>
2020-04-04 22:04:27 +0800

    admin search works

--- src/admin/searchConditions.php
+++ src/admin/searchConditions.php
@@ -1,63 +1,108 @@
 <?php
-include "../connect.php";
-$tables[] = $_POST['tables'];
-$columns[] = $_POST['columns'];
-$sColumns[] = $_POST['sColumns'];
-$cond[] = $_POST["cond"];
-$conn = OpenCon();
-
-$count = 1;
-
-if (isset($_POST["submit"])) {
-    if ($_POST["submit"] == "Add" AND ($cond[$count - 1] == "EXISTS" OR $cond[$count - 1] == "NOT EXISTS")) {
-        $count = count($_POST["sColumns"]) + 1;
-    } else if ($_POST["submit"] == "Add") {
-
-    } else if ($_POST["submit"] == "Done") {
-
-            print "<pre>";
-            print_r($_POST["firstname"]);
-            print_r($_POST["lastname"]);
-            print "</pre>";
-
-        }
+print_r($_POST);
+$tables = $_POST['tables'];
+$columns = $_POST['columns'];
+if (!isset($_POST["selectedColumns"])) {
+    echo 'No columns were selected, please select at least one column. Returning back';
+    header('Refresh: 3; Location: searchColumn.php');
 }
-
+$selectedColumns = $_POST['selectedColumns'];
+$cond = [];
+if (isset($_POST['cond'])) {
+    $cond = $_POST['cond'];
+}
+$queryColumns = [];
+if (isset($_POST['queryColumns'])) {
+    $queryColumns = $_POST['queryColumns'];
+}
+$count = 1;
+if (isset($_POST["submit"]) AND $_POST["submit"] == "Add") {
+    echo count($_POST['queryColumns']);
+    $count = count($_POST["queryColumns"]) + 1;
+} else if (isset($_POST['submit']) AND $_POST["submit"] == "Done") {
+    session_start();
+    $_SESSION['adminQuery'] = $_POST;
+    header('Location: doSearch.php');
+}
 ?>
-<html>
+<html lang="en">
 <body>
 Here are the tables.columns that you have selected:
 <br>
-<?php foreach($columns as $tc){
-    echo $tc." <br>";
-}?>
+<?php foreach ($selectedColumns as $tc) {
+    echo $tc . "<br>";
+} ?>
 Would you like to add any conditions? <br>
 Click on done if you do not want to impose any conditions on this search or if you are done adding conditions.
 <br>
 <form method="post">
-    <?php for($i = 0; $i < $count; $i++) { ?>
+    <?php for ($i = 0; $i < $count; $i++) { ?>
         <p class="field">
-            <label> Condition <?php echo $i ?>
-                <input id="sColumn" type="text" name="sColumns[]" value="<?php print $sColumns[$i]; ?>" />
-                <select id="operator" name="operators[]" value="<">
-                    <option value="=">=</option>
-                    <option value="<"> < </option>
-                    <option value=">"> > </option>
-                    <option value="=<"> =< </option>
-                    <option value="=>"> => </option>
-                    <option value="!="> != </option>
-                    <option value="LIKE"> LIKE </option>
+            Condition <?php echo $i + 1; ?>
+            <?php if (count($queryColumns) > $i) { ?>
+                <select id="queryColumns" name="queryColumns[]">
+                    <?php for ($c = 0; $c < count($columns); $c++) { ?>
+                        <option value="<?php echo $columns[$c]; ?>"
+                            <?php if($queryColumns[$i] == $columns[$c]){ ?>
+                                selected
+                            <?php } ?>>
+                            <?php echo $columns[$c]; ?>
+                        </option>
+                    <?php } ?>
                 </select>
-                <label for="cond">Enter the condition here</label>
-                <input type="text" name="cond[]" id="cond">
-            </label>
-            <?php if ($i < $count - 1) { echo "AND"; } ?>
+            <?php } else { ?>
+                <select id="queryColumn" name="queryColumns[]">
+                    <?php for ($c = 0; $c < count($columns); $c++) { ?>
+                        <option value="<?php echo $columns[$c]; ?>">
+                            <?php echo $columns[$c]; ?>
+                        </option>
+                    <?php } ?>
+                </select>
+            <?php } ?>
+            <select id="operator" name="operators[]" value="operators">
+                <option value="=">
+                    =
+                </option>
+                <option value="<">
+                    <
+                </option>
+                <option value=">">
+                    >
+                </option>
+                <option value="=<">
+                    =<
+                </option>
+                <option value="=>">
+                    =>
+                </option>
+                <option value="!="> !=</option>
+                <option value="LIKE"> LIKE</option>
+            </select>
+            <label for="cond">to</label>
+            <input type="text" name="cond[]" id="cond"
+                   value="<?php if (count($cond) > $i) {
+                       echo $cond[$i];
+                   } else {
+                       echo '';
+                   } ?>">
             <br>
         </p>
     <?php } ?>
     <p>
-        <input type="submit" name="submit" value="Add" />
-        <input type="submit" name="submit" value="Done" />
+        <?php foreach ($tables as $t): ?>
+            <input type="hidden" id="tables" name="tables[]" value="<?php echo $t; ?>"/>
+        <?php endforeach; ?>
+        <?php foreach ($columns as $c): ?>
+            <input type="hidden" id="columns" name="columns[]" value="<?php echo $c; ?>"/>
+        <?php endforeach; ?>
+        <?php foreach ($selectedColumns as $sc): ?>
+            <input type="hidden" id="selectedColumns" name="selectedColumns[]" value="<?php echo $sc; ?>"/>
+        <?php endforeach; ?>
+<!--        --><?php //foreach ($queryColumns as $qc): ?>
+<!--            <input type="hidden" id="queryColumns" name="queryColumns[]" value="--><?php //echo $qc; ?><!--"/>-->
+<!--        --><?php //endforeach; ?>
+        <input type="submit" name="submit" value="Add"/>
+        <input type="submit" name="submit" value="Done"/>
     </p>
 </form>
 
