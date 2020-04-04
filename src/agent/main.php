9cea72803d8fef0032dab2cb031c64d334bff378
Jonathan Kim <jonathan.sj.kim@gmail.com>
2020-04-03 19:15:11 -0700

    with index changed and agent main added

---
+++ src/agent/main.php
@@ -0,0 +1,24 @@
+<?php
+function login($conn, $sql, $username)
+{
+    $result = mysqli_query($conn, $sql);
+    if (!$result) {
+        header('Location: ../../html/failedLogin.html');
+    }
+    $rows = mysqli_fetch_assoc($result);
+    $name = array_values($rows)[0];
+    if ($name) {
+        header('Location: welcome.php?name='.urlencode($name).'&username='.urlencode($username));
+    } else {
+        header('Location: ../main/failedLogin.html');
+    }
+    CloseCon($conn);
+}
+include '../connect.php';
+echo 'Logging in...';
+$username = $_POST['username'];
+$password = $_POST['password'];
+$connection = OpenCon();
+$sql = "SELECT u.Name FROM agent u WHERE u.Username = '$username' AND u.Password = '$password'";
+login($connection, $sql, $username);
+CloseCon($connection);
