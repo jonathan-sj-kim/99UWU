f67810243fe2b517f63244588f6802ecbaac040f
y0q1b <y0q1b@ugrad.cs.ubc.ca>
2020-04-04 19:16:29 -0700

    more css

--- src/traveller/welcome.php
+++ src/traveller/welcome.php
@@ -3,17 +3,29 @@
 <head>
     <meta charset="UTF-8">
     <title>Welcome Page for Traveller</title>
+    <link rel="stylesheet" type="text/css" href="travellermain.css">
+
 </head>
 <body>
-Welcome Traveller <?php echo $_GET["name"]; ?>!
-</br>
-What you you like to do?
-</br>
+<h1>
+    Welcome Traveller <?php echo $_GET["name"]; ?>!
+</h1>
+<h3> What would you like to do today? </h3>
+
 <form action="processTravellerAction.php" method="post">
+    Please choose the service you are looking for from below
+
+
     <input name="username" type="hidden" id="hiddenUserName" value="<?php echo $_GET['username']; ?>" />
     <input name="user-input" id="search-listing-btn" type="submit" value="Search for listings"/>
     <input name="user-input" id="booking-btn" type = "submit" value = "See booking history" />
-</form>
 
+    </div>
+
+    <div class="nextstep">
+        <a href = "../index.html">Back to home page</a>
+    </div>
+
+</form>
 </body>
 </html>
